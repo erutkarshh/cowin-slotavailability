a4b29a5ef33923e068d3fdc0e7bfcaad389f3430
erutkarshh <38435414+erutkarshh@users.noreply.github.com>
2021-05-23 00:26:06 +0530

    Update AvailableSlots.ps1

--- AvailableSlots.ps1
+++ AvailableSlots.ps1
@@ -1,3 +1,62 @@
+# Display Vaccination Centre Basic Info
+function ShowVaccinationCenterBasicInformation($session){
+	write-host "Block:"$session.block_name",	Pin Code:"$session.pincode", Vaccine:"$session.Vaccine",	Fee(Rupees):"$session.Fee",	" -nonewline
+}
+
+# Display Vaccination Centre
+function ShowVaccinationCenter($doseType, $session, [int]$dataCount) {
+	if($doseType -eq '1' -and $session.available_capacity_dose1 -ne '0')
+	{
+		ShowVaccinationCenterBasicInformation $session
+		write-host "Available (1st Dose): "$session.available_capacity_dose1 -ForegroundColor White -BackgroundColor DarkGreen						
+	       
+		write-host "======================================================="		
+		
+		# increment data count
+		$dataCount = $dataCount + 1
+	}
+	elseif($doseType -eq '2' -and $session.available_capacity_dose2 -ne '0')
+	{
+		ShowVaccinationCenterBasicInformation $session
+		write-host "Available (2nd Dose): "$session.available_capacity_dose2 -ForegroundColor White -BackgroundColor DarkGreen
+		       
+		write-host "======================================================="
+
+		# increment data count
+		$dataCount = $dataCount + 1
+	}
+	
+	return $dataCount
+}
+
+# Display Vaccination Centre, along with Address information
+function ShowVaccinationCenterWithAddress($doseType, $session, [int]$dataCount) {
+	if($doseType -eq '1' -and $session.available_capacity_dose1 -ne '0')
+	{
+		ShowVaccinationCenterBasicInformation $session
+		write-host "Available (1st Dose): "$session.available_capacity_dose1 -ForegroundColor White -BackgroundColor DarkGreen
+		write-host "Address: "$session.address"" -ForegroundColor White
+		      
+		write-host "======================================================="
+
+		# increment data count
+		$dataCount = $dataCount + 1
+	}
+	elseif($doseType -eq '2' -and $session.available_capacity_dose2 -ne '0')
+	{
+		ShowVaccinationCenterBasicInformation $session
+		write-host "Available (2nd Dose): "$session.available_capacity_dose2 -ForegroundColor White -BackgroundColor DarkGreen
+		write-host "Address: "$session.address"" -ForegroundColor White
+		       
+		write-host "======================================================="
+
+		# increment data count
+		$dataCount = $dataCount + 1
+	}
+	
+	return $dataCount
+}
+
 $date = Read-Host "Please enter desired slot date (dd-mm-yyyy)"#"06-05-2021"
 $minAgeLimit = Read-Host "Please enter age group (18 or 45)" # 18 or 45
 $waitSeconds = 10
@@ -67,101 +126,96 @@ try
 	}while($flag -eq 1)
 
 	# post code
-    $pincodeChoice = Read-Host "Please enter choice of pincode(s)(411026,411027,411028 or default is all)"
+    $pincodeChoice = Read-Host "Please enter choice of pincode(s)(411026,411027,411028 or default is ALL)"
     if($pincodeChoice -eq '')
     {
 	    $pincodeChoice = "ALL"
     }
+	Write-Host "Selected Pincode(s): " -nonewline
+	Write-Host $pincodeChoice -ForegroundColor White -BackgroundColor DarkGreen
 	
 	# dose type
-	$doseType = Read-Host "Please enter choice of dose (for 1st dose press 1, for 2nd dose press 2, default is 1st dose)"
+	$doseType = Read-Host "Please enter choice of Dose (1 = 1st Dose, 2 = 2nd Dose, default is ANY)"
     if($doseType -ne '1' -and $doseType -ne '2')
     {
 	    $doseType = "1"
     }
-	Write-Host "DoseSelected: " -nonewline
+	Write-Host "Selected Dose Type: " -nonewline
 	Write-Host $doseType -ForegroundColor White -BackgroundColor DarkGreen
+	
+	# Vaccine type
+	$vaccineType = Read-Host "Please enter choice of Vaccine (1 = COVAXIN, 2 = COVISHIELD, default is ANY)"
+	if($vaccineType -eq '1'){
+		$vaccineType = "COVAXIN"
+	} elseif ($vaccineType -eq '2') {
+		$vaccineType = "COVISHIELD"
+	} else {
+		$vaccineType = "ANY"
+	}
+	Write-Host "Selected Vaccine: " -nonewline
+	Write-Host $vaccineType -ForegroundColor White -BackgroundColor DarkGreen
 	
 	# find slots
 	do
 	{
-	$currentTime = Get-Date -Format "dd/MMM/yyyy HH:mm:ss"
-	$uri = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=$districtId&date=$date"
-	$webData = Invoke-RestMethod -Uri $uri
-	$dataCount = 0
-	if($webData.sessions -ne $null -and $webData.sessions.Count -gt 0)
-	{
-
-		foreach($session in $webData.sessions)
-		{			
-			if($session.min_age_limit -eq $null -OR $session.min_age_limit -le $minAgeLimit)
-			{
-                if($pincodeChoice -eq "ALL")
-                {				    
-					if($doseType -eq '1' -and $session.available_capacity_dose1 -ne '0')
+		$currentTime = Get-Date -Format "dd/MMM/yyyy HH:mm:ss"
+		$uri = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=$districtId&date=$date"
+		$webData = Invoke-RestMethod -Uri $uri
+		$dataCount = 0
+		if($webData.sessions -ne $null -and $webData.sessions.Count -gt 0)
+		{
+			foreach($session in $webData.sessions)
+			{			
+				if($session.min_age_limit -eq $null -OR $session.min_age_limit -le $minAgeLimit)
+				{
+					if($vaccineType -eq 'COVAXIN' -or $vaccineType -eq 'COVISHIELD') 
 					{
-						write-host "Block:"$session.block_name",	Pin Code:"$session.pincode", Vaccine:"$session.Vaccine",	Fee(Rupees):"$session.Fee",	" -nonewline
-						write-host "Available (1st Dose): "$session.available_capacity_dose1 -ForegroundColor White -BackgroundColor DarkGreen						
-					
-						$dataCount = $dataCount + 1        
-						write-host "======================================================="
-					}
-					elseif($doseType -eq '2' -and $session.available_capacity_dose2 -ne '0')
-					{
-						write-host "Block:"$session.block_name",		Pin Code:"$session.pincode", Vaccine:"$session.Vaccine",	Fee(Rupees):"$session.Fee",	" -nonewline
-						write-host "Available (2nd Dose): "$session.available_capacity_dose2 -ForegroundColor White -BackgroundColor DarkGreen
-						
-						$dataCount = $dataCount + 1        
-						write-host "======================================================="
-					}
-                }
-                else
-                {
-                    if($pincodeChoice.contains($session.pincode))
-                    {       
-				        if($doseType -eq '1' -and $session.available_capacity_dose1 -ne '0')
+						if($vaccineType.contains($session.vaccine)) 
 						{
-							write-host "Block:"$session.block_name",		Pin Code:"$session.pincode", Vaccine:"$session.Vaccine",	Fee(Rupees):"$session.Fee",	" -nonewline
-							write-host "Available (1st Dose): "$session.available_capacity_dose1 -ForegroundColor White -BackgroundColor DarkGreen
-							write-host "Address: "$session.address"" -ForegroundColor White
-							
-							$dataCount = $dataCount + 1        
-							write-host "======================================================="
+							if($pincodeChoice -eq "ALL")
+							{				    
+								$dataCount = ShowVaccinationCenter $doseType $session $dataCount								
+							}
+							elseif($pincodeChoice.contains($session.pincode))
+							{
+								$dataCount = ShowVaccinationCenterWithAddress $doseType $session $dataCount
+							}
 						}
-						elseif($doseType -eq '2' -and $session.available_capacity_dose2 -ne '0')
+					}
+					else
+					{
+						if($pincodeChoice -eq "ALL")
+						{				    
+							$dataCount = ShowVaccinationCenter $doseType $session $dataCount
+						}
+						elseif($pincodeChoice.contains($session.pincode))
 						{
-							write-host "Block:"$session.block_name",		Pin Code:"$session.pincode", Vaccine:"$session.Vaccine",	Fee(Rupees):"$session.Fee",	" -nonewline
-							write-host "Available (2nd Dose): "$session.available_capacity_dose2 -ForegroundColor White -BackgroundColor DarkGreen
-							write-host "Address: "$session.address"" -ForegroundColor White
-							
-							$dataCount = $dataCount + 1        
-							write-host "======================================================="
-						} 
-                    }
-                }
-			} 
-          
-		}    
-	}
+							$dataCount = ShowVaccinationCenterWithAddress $doseType $session $dataCount
+						}
+					}
+				} 
+			  
+			}    
+		}
 
-	if($dataCount -eq 0)
-	{
-		Write-Host "No slot available." -ForegroundColor White -BackgroundColor Red
-	}
-	else
-	{
-		[console]::beep(3000,500)
-	}
+		if($dataCount -eq 0)
+		{
+			Write-Host "No slot available." -ForegroundColor White -BackgroundColor Red
+		}
+		else
+		{
+			[console]::beep(3000,500)
+		}
 
-	Write-Host
-	Write-Host "LastRefresh: $currentTime, Age Group: $minAgeLimit Years, For date: $date, Next Refresh In: $waitSeconds Seconds" -ForegroundColor Black -BackgroundColor Gray
-	Write-Host "---------------------------------------------------------"
-	Start-Sleep -s $waitSeconds
+		Write-Host
+		Write-Host "LastRefresh: $currentTime, Age Group: $minAgeLimit Years, Date: $date, Dose:$doseType, Vaccine:$vaccineType, Next Refresh In: $waitSeconds Seconds" -ForegroundColor Black -BackgroundColor Gray
+		Write-Host "---------------------------------------------------------"
+		Start-Sleep -s $waitSeconds
 	}while($flag -eq 2)
 
 }
 catch [Exception]{
-	Write-Host "An Error Occurred while calling API. $_" -ForegroundColor Red
+	Write-Host "An Error Occurred. $_" -ForegroundColor Red
 	[console]::beep(1000,300)
 }
 finally{	
